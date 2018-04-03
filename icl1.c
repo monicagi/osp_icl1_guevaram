771b0fc943b9de79e84c62c1bb9a03e11030e523
Guevara <guevaram@debian-9.3.0-template>
2018-04-03 12:31:42 -0700

    icl1.c modified with file i/o

--- icl1.c
+++ icl1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+	printf("Hello World!!!");
+	int fd = open("/tmp/guevaram.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+	assert(fd > -1);
+	int rc = write(fd, "Quote: hi world\n", 17);
+	assert(rc == 17);
+	close(fd);
+	
+	return 0;
 }
